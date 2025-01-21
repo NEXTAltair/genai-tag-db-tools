848de97543861e4069a9acfe8f443ddc92ad7d0b
NEXTAltair <1401591+NEXTAltair@users.noreply.github.com>
2025-01-21 13:54:28 +0900

    不要な引数が必要だったのを削除

--- genai_tag_db_tools/main.py
+++ genai_tag_db_tools/main.py
@@ -2,14 +2,10 @@ import sys
 
 from PySide6.QtWidgets import QApplication
 from genai_tag_db_tools.gui.windows.main_window import MainWindow
-from genai_tag_db_tools.services.tag_search import TagSearcher
-
 
 def main():
     app = QApplication(sys.argv)
-    tag_searcher = TagSearcher()
     window = MainWindow()
-    window.initialize(tag_searcher)
     window.show()
     sys.exit(app.exec())
 
